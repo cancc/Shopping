563e2e4e3cf707af0a4238b3f8fb3e9933aa6589
cancc <cancc@users.noreply.github.com>
2017-03-21 21:10:50 +0800

    Create nav.js

---
+++ scripts/nav.js
@@ -0,0 +1,8 @@
+//导航效果
+$(function(){
+	$("#navigation ul li:has(ul)").hover(function(){
+		$(this).children("ul").stop(true,true).slideDown(400);
+	},function(){
+		$(this).children("ul").stop(true,true).slideUp();
+	});
+})
